d98648900207610906374c1d2c452d7148ec94a9
Jogo1001 <101247726@georgebrown.ca>
2025-09-16 14:56:28 -0400

    adding partyserializer/deserialize script

---
+++ Assets/PartySerializer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PartySerializer : MonoBehaviour
+{
+    public static string SerializeParty(string partyName, IEnumerable<PartyCharacter> characters)
+    {
+
+
+        return;
+    }
+
+    public static (string partyName, LinkedList<PartyCharacter> characters) DeserializeParty(string fileContent)
+    {
+
+    }
+}
